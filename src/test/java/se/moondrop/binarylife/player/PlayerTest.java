8c9e9538e656628833cdd709bbc7d2cc057b3f65
sandmaster <robin.ejderholt@gmail.com>
2021-07-01 17:24:55 +0200

    Test class for the Player class

---
+++ src/test/java/se/moondrop/binarylife/player/PlayerTest.java
@@ -0,0 +1,22 @@
+package se.moondrop.binarylife.player;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+
+	@BeforeEach
+	void setup() {
+		Player mockPlayer = new Player();
+
+	}
+
+	@Test
+	void testIncreaseLevel() {
+		fail("Not yet implemented");
+
+	}
+
+}
